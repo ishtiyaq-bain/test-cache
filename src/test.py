fccbb55f07e44e8292fadea6ecb021f6b631a251
ishtiyaq-bain <142508043+ishtiyaq-bain@users.noreply.github.com>
2023-11-08 15:26:31 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,9 +1,10 @@
+import os
 
-# Open a file for writing (creates the file if it doesn't exist)
-with open('output.txt', 'w') as file:
-    # Write text to the file
+file_path = os.environ.get('MY_VARIABLE')
+
+with open(file_path, 'w') as file:
     file.write('This is a simple text file.\n')
     file.write('You can write multiple lines like this.\n')
     file.write('And close the file when you are done.\n')
 
-print('Text file "output.txt" has been created and written to.')
+print(f"Text file {file_path} has been created and written to.")
