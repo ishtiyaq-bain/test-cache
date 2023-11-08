e84529dc91af923ad55ac55ae04e964e4e6d36aa
ishtiyaq-bain <142508043+ishtiyaq-bain@users.noreply.github.com>
2023-11-08 15:30:00 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ import os
     
 file_path = os.environ.get('RUN_ID')
 
-with open(file_path, 'w') as file:
+with open(f"{file_path}.txt", 'w') as file:
     file.write('This is a simple text file.\n')
     file.write('You can write multiple lines like this.\n')
     file.write('And close the file when you are done.\n')
